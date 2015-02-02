cf66a0eac6406cb711c3a18f74a99d79b2578045
Jennie Lees <jennie@nilas.com>
2015-02-02 15:58:02 -0800

    Don't try to convert nil times

--- lib/time_attr_accessor.rb
+++ lib/time_attr_accessor.rb
@@ -3,7 +3,9 @@ module Inbox
     def time_attr_accessor(attr)
       parameter attr
       define_method "#{attr}=" do |value|
-        instance_variable_set "@#{attr}", Time.at(value).utc
+        if value
+            instance_variable_set "@#{attr}", Time.at(value).utc
+        end
       end
     end
   end
