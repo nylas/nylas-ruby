63691d8d660ffdf85391d1de520d12028e4f8440
Steven Harman <steven@harmanly.com>
2015-09-23 15:33:16 -0400

    Return an external Enumerator when no block given
    Closes #41

--- spec/restful_model_collection_spec.rb
+++ spec/restful_model_collection_spec.rb
@@ -7,6 +7,26 @@ describe 'RestfulModelCollection' do
     @api = Inbox::API.new(@app_id, @app_secret, @access_token)
   end
 
+  describe '#each' do
+    before do
+      stub_request(:get, "https://#{@access_token}:@api.nylas.com/threads?limit=100&offset=0").
+        to_return(:status => 200,
+                  :body => File.read('spec/fixtures/threads_reply.txt'),
+                  :headers => {"Content-Type" => "application/json"})
+    end
+
+    it 'returns an external Enumerator when no block is given' do
+      expect(@api.threads.each).to be_a(Enumerator)
+      expect(@api.threads.each.map {|t| t.id }).to eq(['320yc9ie5rungx75j139dsgao', 'vxz6vx6rm2imw0mow9rc1prk'])
+    end
+
+    it 'yields the individual threads when a block is given' do
+      thread_ids = []
+      @api.threads.each { |t| thread_ids << t.id }
+      expect(thread_ids).to eq(['320yc9ie5rungx75j139dsgao', 'vxz6vx6rm2imw0mow9rc1prk'])
+    end
+  end
+
   describe '#count' do
     it 'should return number of entities' do
       stub_request(:get, "https://#{@access_token}:@api.nylas.com/threads?view=count").to_return(
