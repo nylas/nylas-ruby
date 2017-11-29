90cdc420b2647a355afa7d46dd77ab6bfcf2ad16
Zee Spencer <zee@zincma.de>
2017-11-28 17:30:25 -0800

    Remove implicit dependency on ActiveSupport
    For some reason, we had a require for `active_support` in lib/nylas,
    which meant that even though we didn't declare a dependency on
    ActiveSupport, we assumed that you would be using it.
    
    This removes that in favor of a small class that casts hashes to query
    strings.
    
    We may be able to get rid of this if we leverage RestClient for
    URL building.

---
+++ lib/nylas/to_query.rb
@@ -0,0 +1,16 @@
+module Nylas
+  class ToQuery
+    attr_accessor :hash
+    def initialize(hash)
+      self.hash = hash
+    end
+
+    def to_s
+      hash.reduce("") do |query, (key, value)|
+        next query if value.nil?
+        pair = "#{key}=#{URI.escape(value.to_s)}"
+        query.empty? ? "#{query}#{pair}" : "#{query}&#{pair}"
+      end
+    end
+  end
+end
