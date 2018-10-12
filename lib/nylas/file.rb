0eede96b94af2346edf622aa3c2d74c52e014786
Michael Deryugin <Michael.Deryugin@gmail.com>
2018-10-12 17:15:00 +0400

    fix encoding for tempfile when downloading attachments

--- lib/nylas/file.rb
+++ lib/nylas/file.rb
@@ -49,7 +49,7 @@ module Nylas
     private def retrieve_file
       response = api.get(path: "#{resource_path}/download")
       filename = response.headers.fetch(:content_disposition, "").gsub("attachment; filename=", "")
-      temp_file = Tempfile.new(filename)
+      temp_file = Tempfile.new(filename, :encoding => 'ascii-8bit')
       temp_file.write(response.body)
       temp_file.seek(0)
       temp_file
