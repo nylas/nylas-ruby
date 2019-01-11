42ddab3338c599b5977a9c0f081014db5cb47748
Christopher Che <cmche0221@gmail.com>
2019-01-11 08:58:49 -0500

    Spec fixes after adding default values

--- spec/nylas/contact_spec.rb
+++ spec/nylas/contact_spec.rb
@@ -7,9 +7,12 @@ describe Nylas::Contact do
       '"birthday": "1984-01-01", "suffix": "Jr.", "nickname": "nick", ' \
       '"company_name": "company", "job_title": "title", ' \
       '"manager_name": "manager", "office_location": "the office", ' \
+      '"picture_url": "", ' \
+      '"source": "", ' \
+      '"web_page": { "type": "", "url": "" }, ' \
       '"notes": "some notes", "emails": [' \
-        '{ "type": "work", "email": "given@work.example.com" }, ' \
-        '{ "type": "home", "email": "given@home.example.com" }], ' \
+        '{ "type": "work", "email": "given@work.example.com", "name": "" }, ' \
+        '{ "type": "home", "email": "given@home.example.com", "name": "" }], ' \
       '"im_addresses": [{ "type": "gtalk", "im_address": "given@gtalk.example.com" }],' \
       '"physical_addresses": [{ "format": "structured", "type": "work",' \
         '"street_address": "123 N West St", "postal_code": "12345+0987", "city": "Los Angeles", "state": "CA",' \
@@ -53,9 +56,11 @@ describe Nylas::Contact do
       expected_payload = JSON.dump(given_name: "Given",
                                    birthday: "2017-01-01",
                                    emails: [{ type: "work",
-                                              email: "given@other-job.example.com" },
+                                              email: "given@other-job.example.com",
+                                              name: "" },
                                             { type: "home",
-                                              email: "given@other-home.example.com" }])
+                                              email: "given@other-home.example.com",
+                                              name: "" }])
       expect(request[:method]).to be :put
       expect(request[:path]).to eql "/contacts/1234"
       expect(request[:payload]).to eql(expected_payload)
@@ -116,8 +121,11 @@ describe Nylas::Contact do
                                   birthday: "1984-01-01",
                                   company_name: "company",
                                   notes: "some notes",
-                                  emails: [{ type: "work", email: "given@work.example.com" },
-                                           { type: "home", email: "given@home.example.com" }],
+                                  picture_url: "",
+                                  source: "",
+                                  web_page: { type: "", url: "" },
+                                  emails: [{ type: "work", email: "given@work.example.com", name: "" },
+                                           { type: "home", email: "given@home.example.com", name: "" }],
                                   im_addresses: [{ type: "gtalk", im_address: "given@gtalk.example.com" }],
                                   phone_numbers: [{ type: "mobile", number: "+1234567890" }],
                                   physical_addresses: [{ format: "structured", type: "work",
@@ -143,6 +151,15 @@ describe Nylas::Contact do
                                   manager_name: "manager",
                                   birthday: "1984-01-01",
                                   company_name: "company",
+                                  picture_url: "",
+                                  source: "",
+                                  web_page: "",
+                                  emails: [],
+                                  groups: [],
+                                  im_addresses: [],
+                                  notes: "",
+                                  web_pages: [],
+                                  phone_numbers: [],
                                   physical_addresses: [{ format: "structured", type: "work",
                                                          street_address: "123 N West St",
                                                          postal_code: "", city: "", state: "",
