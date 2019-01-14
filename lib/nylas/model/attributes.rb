cb666501265f7dbb4104779a23ccb3a036b73be6
Christopher Che <cmche0221@gmail.com>
2019-01-14 08:48:14 -0500

    No more default values, just set value if not nil
    Add value variable back

--- lib/nylas/model/attributes.rb
+++ lib/nylas/model/attributes.rb
@@ -33,7 +33,7 @@ module Nylas
       def to_h(keys: attribute_definitions.keys)
         keys.each_with_object({}) do |key, casted_data|
           value = attribute_definitions[key].serialize(self[key])
-          casted_data[key] = defaulted_value(value)
+          casted_data[key] = value unless value.nil?
         end
       end
 
@@ -43,13 +43,6 @@ module Nylas
 
       private def default_attributes
         attribute_definitions.keys.zip([]).to_h
-      end
-
-      private def defaulted_value(value)
-        result = value unless value.nil? || (value.respond_to?(:empty?) && value.empty?)
-        result ||= [] if value.is_a?(Array)
-        result ||= "" unless [true, false].include?(value)
-        result
       end
     end
   end
