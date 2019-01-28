bc49910a97a81e9c2d05da72379c889091188650
Christopher Che <cmche0221@gmail.com>
2019-01-28 11:09:32 -0500

    Add missing attributes to timespan

--- lib/nylas/timespan.rb
+++ lib/nylas/timespan.rb
@@ -8,6 +8,10 @@ module Nylas
     attribute :object, :string
     attribute :start_time, :unix_timestamp
     attribute :end_time, :unix_timestamp
+    attribute :time, :unix_timestamp
+    attribute :date, :unix_timestamp
+    attribute :start_date, :unix_timestamp
+    attribute :end_date, :unix_timestamp
 
     def_delegators :range, :cover?
 
