fb99e7afa04a0cb8bb5ce2603849013138e4a1fe
Amanda Cameron <amandacameron@ip-192-168-1-214.us-west-2.compute.internal>
2019-03-26 13:58:54 -0700

    changed ip_address endpoint, tested locally

--- lib/nylas/api.rb
+++ lib/nylas/api.rb
@@ -103,7 +103,8 @@ module Nylas
     # @return [Hash]
     # hash has keys of :updated_at (unix timestamp) and :ip_addresses (array of strings)
     def ip_addresses
-        response = client.as(access_token).get(path: "/ip_addresses")
+        path = "/a/"+app_id+"/ip_addresses"
+        response = client.as(client.app_secret).get(path: path)
         return response
     end
 
