e8d4ee7bedbe47c97fcde04d89c1326c3ac3fb2a
Arun Agrawal <arunagw@gmail.com>
2019-08-18 23:39:30 +0530

    Better indent

--- spec/nylas/thread_spec.rb
+++ spec/nylas/thread_spec.rb
@@ -67,14 +67,23 @@ describe Nylas::Thread do
     it "let's you set the starred, unread, folder, and label ids" do
       api =  instance_double(Nylas::API, execute: "{}")
       thread = described_class.from_json('{ "id": "thread-1234" }', api: api)
-      thread.update(starred: true, unread: false, folder_id: "folder-1234",
-                    label_ids: %w[label-1234 label-4567])
 
-      expect(api).to have_received(:execute).with(method: :put, path: "/threads/thread-1234",
-                                                  payload: JSON.dump(starred: true, unread: false,
-                                                                     folder_id: "folder-1234",
-                                                                     label_ids: %w[label-1234
-                                                                                   label-4567]))
+      thread.update(
+        starred: true,
+        unread: false,
+        folder_id: "folder-1234",
+        label_ids: %w[label-1234 label-4567]
+      )
+
+      expect(api).to have_received(:execute).with(
+        method: :put, path: "/threads/thread-1234",
+        payload: JSON.dump(
+          starred: true, unread: false,
+          folder_id: "folder-1234",
+          label_ids: %w[label-1234
+                        label-4567]
+        )
+      )
     end
 
     it "raises an argument error if the data has any keys that aren't allowed to be updated" do
