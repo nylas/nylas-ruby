41d16edd2ad39eeed3511d85f51edb397f89db7b
Arun Agrawal <arunagw@gmail.com>
2019-09-24 07:37:22 +0530

    Allow contact groups to be fetched

--- lib/nylas/contact_group.rb
+++ lib/nylas/contact_group.rb
@@ -4,7 +4,15 @@ module Nylas
   # Structure to represent the Contact Group schema
   # @see https://docs.nylas.com/reference#contactsid
   class ContactGroup
-    include Model::Attributable
+    include Model
+    self.resources_path = "/contacts/groups"
+    self.creatable = false
+    self.listable = true
+    self.showable = false
+    self.filterable = false
+    self.updatable = false
+    self.destroyable = false
+
     attribute :id, :string
     attribute :object, :string
     attribute :account_id, :string
