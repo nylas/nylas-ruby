36982572b4269b346d4629f37028fb2e2268448f
Nick Bair <nick.b@nylas.com>
2020-12-29 16:45:34 -0700

    Skip parsing response if content is not JSON.

--- lib/nylas/http_client.rb
+++ lib/nylas/http_client.rb
@@ -2,6 +2,7 @@
 
 module Nylas
   require "yajl"
+  require "pry"
 
   # Plain HTTP client that can be used to interact with the Nylas API sans any type casting.
   class HttpClient # rubocop:disable Metrics/ClassLength
@@ -82,7 +83,14 @@ module Nylas
         timeout: timeout
       )
       rest_client_execute(**request) do |response, _request, result|
-        response = parse_response(response)
+        content_type = nil
+
+        if response.headers && response.headers[:content_type]
+          content_type = response.headers[:content_type].downcase
+        end
+
+        response = parse_response(response) if content_type == "application/json"
+
         handle_failed_response(result: result, response: response)
         response
       end
--- spec/nylas/http_client_spec.rb
+++ spec/nylas/http_client_spec.rb
@@ -2,12 +2,25 @@
 
 require "spec_helper"
 require "yajl"
+require "webmock/rspec"
 
 describe Nylas::HttpClient do
   let(:full_json) do
     '{"snippet":"\u26a1\ufe0f Some text \ud83d","starred":false,"subject":"Updates"}'
   end
-  let(:api) { FakeAPI.new }
+
+  let(:headers) do
+    {
+      "Accept" => "*/*",
+      "Accept-Encoding" => "gzip;q=1.0,deflate;q=0.6,identity;q=0.3",
+      "Authorization" => "Basic dG9rZW46",
+      "Content-Types" => "application/json",
+      "Host" => "api.nylas.com",
+      "User-Agent" => "Nylas Ruby SDK 4.6.2 - 2.7.0",
+      "X-Nylas-Api-Wrapper" => "ruby",
+      "X-Nylas-Client-Id" => "id"
+    }
+  end
 
   describe "#parse_response" do
     it "deserializes JSON with unicode characters" do
@@ -22,4 +35,27 @@ describe Nylas::HttpClient do
     end
   end
 
+  describe "#execute handles content types" do
+    it "parses JSON when given content-type == application/json" do
+      nylas = described_class.new(app_id: "id", app_secret: "secret", access_token: "token")
+
+      stub_request(:get, "https://api.nylas.com/contacts/1234")
+        .with(headers: headers)
+        .to_return(status: 200, body: full_json, headers: { "Content-Type" => "Application/Json" })
+
+      response = nylas.execute(method: :get, path: "/contacts/1234")
+      expect(response).not_to be_a_kind_of(String)
+    end
+
+    it "skips when content-type is not JSON" do
+      nylas = described_class.new(app_id: "id", app_secret: "secret", access_token: "token")
+
+      stub_request(:get, "https://api.nylas.com/contacts/1234/picture")
+        .with(headers: headers)
+        .to_return(status: 200, body: "some values", headers: { "Content-Type" => "image/jpeg" })
+
+      response = nylas.execute(method: :get, path: "/contacts/1234/picture")
+      expect(response).to be_a_kind_of(String)
+    end
+  end
 end
