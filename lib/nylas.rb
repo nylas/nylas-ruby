0e7a30aa280a5dc9775bc6337cf939a3dae510e9
Brad Robertson <brad@platformgroup.ca>
2021-02-12 17:12:34 -0500

    Fix thread-safety issue in HTTP::CookieJar loading

--- lib/nylas.rb
+++ lib/nylas.rb
@@ -3,6 +3,21 @@
 require "json"
 require "rest-client"
 
+# BUGFIX
+#   See https://github.com/sparklemotion/http-cookie/issues/27
+#   and https://github.com/sparklemotion/http-cookie/issues/6
+#
+# CookieJar uses unsafe class caching for dynamically loading cookie jars
+# If 2 rest-client instances are instantiated at the same time, (in threads)
+# non-deterministic behaviour can occur whereby the Hash cookie jar isn't
+# properly loaded and cached.
+# Forcing an instantiation of the jar onload will force the CookieJar to load
+# before the system has a chance to spawn any threads.
+# Note this should technically be fixed in rest-client itself however that
+# library appears to be stagnant so we're forced to fix it here
+# This object should get GC'd as it's not referenced by anything
+HTTP::CookieJar.new
+
 require "ostruct"
 require "forwardable"
 
