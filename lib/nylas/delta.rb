b902e52dcdb99a55257adb0d173c759b68e63ab6
Arun Agrawal <arunagw@gmail.com>
2021-05-12 10:21:03 +0530

    Make it private

--- lib/nylas/delta.rb
+++ lib/nylas/delta.rb
@@ -25,6 +25,8 @@ module Nylas
       @model ||= Types.registry[object.to_sym].cast(object_attributes_with_ids)
     end
 
+    private
+
     def object_attributes_with_ids
       (object_attributes || {}).merge(id: id, account_id: account_id)
     end
