fbc932b7ec8a26710ac85895ccfac7313ece824f
Mostafa Rashed <17770919+mrashed-dev@users.noreply.github.com>
2021-10-28 12:12:08 -0400

    Update comments in example

--- examples/plain-ruby/scheduler.rb
+++ examples/plain-ruby/scheduler.rb
@@ -1,8 +1,8 @@
 require_relative '../helpers'
 
 # An executable specification that demonstrates how to use the Nylas Ruby SDK to interact with the Nylas
-# Events API.
-# See https://docs.nylas.com/reference#events
+# Scheduler API.
+# See https://developer.nylas.com/docs/api/scheduler/#overview
 nylas = Nylas::API.new(app_id: ENV['NYLAS_APP_ID'], app_secret: ENV['NYLAS_APP_SECRET'],
                      access_token: ENV['NYLAS_ACCESS_TOKEN'])
 
