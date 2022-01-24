5e59558bea63935004eface4315a52737b3cd9ca
Mostafa Rashed <17770919+mrashed-dev@users.noreply.github.com>
2022-01-24 16:27:48 -0500

    Modify `Webhook` to support POST, PUT, DELETE
    Override the `save` and `update` methods to ensure that only `status` gets updated on update calls. Marked all properties not used by a `POST` call to read_only.

--- lib/nylas/webhook.rb
+++ lib/nylas/webhook.rb
@@ -5,17 +5,52 @@ module Nylas
   # @see https://docs.nylas.com/reference#webhooks
   class Webhook
     include Model
-    allows_operations(listable: true, showable: true)
-    attribute :id, :string
-    attribute :application_id, :string
+    allows_operations(creatable: true, listable: true, showable: true, updatable: true,
+                      destroyable: true)
+    attribute :id, :string, read_only: true
+    attribute :application_id, :string, read_only: true
 
     attribute :callback_url, :string
     attribute :state, :string
-    attribute :version, :string
+    attribute :version, :string, read_only: true
     has_n_of_attribute :triggers, :string
+
+    def save
+      result = if persisted?
+                 update_call(update_payload)
+               else
+                 create
+               end
+
+      attributes.merge(result)
+    end
+
+    def save_all_attributes
+      save
+    end
+
+    def update(**data)
+      raise ArgumentError, "Only 'state' is allowed to be updated" if data.length > 1 || !data.key?(:state)
+
+      attributes.merge(**data)
+      payload = JSON.dump(data)
+      update_call(payload)
+
+      true
+    end
+
+    def update_all_attributes(**data)
+      update(**data)
+    end
 
     def self.resources_path(api:)
       "/a/#{api.app_id}/webhooks"
+    end
+
+    private
+
+    def update_payload
+      JSON.dump({ state: state })
     end
   end
 end
