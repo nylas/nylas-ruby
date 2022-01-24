72c445ea3e529edec690eddb545339f93d02b5a9
Mostafa Rashed <17770919+mrashed-dev@users.noreply.github.com>
2022-01-24 16:49:08 -0500

    Update webhook example to reflect new support
    Show that now we can create, update, and delete a webhook

--- examples/plain-ruby/webhooks.rb
+++ examples/plain-ruby/webhooks.rb
@@ -7,8 +7,23 @@ api = Nylas::API.new(app_id: ENV['NYLAS_APP_ID'], app_secret: ENV['NYLAS_APP_SEC
 
 # Webhooks can be retrieved as a collection
 demonstrate { api.webhooks.map(&:to_h) }
+
 # Or independently
 example_webhook = api.webhooks.first
 demonstrate { api.webhooks.find(example_webhook.id).to_h }
 
+# Create a webhook
+demonstrate do
+  api.webhooks.create(
+    callback_url: ENV['NYLAS_WEBHOOK_URL'],
+    state: "active",
+    triggers: ["event.created"],
+  )
+end
 
+# Update the status of the webhook
+created_webhook = api.webhooks.last
+demonstrate { created_webhook.update(state: "inactive") }
+
+# Delete webhook
+demonstrate { created_webhook.destroy }
