5de2204def5bab09d705966e621e38bd0ab5936e
Mostafa Rashed <17770919+mrashed-dev@users.noreply.github.com>
2022-01-24 17:34:04 -0500

    [76807] Improve webhook support (#349)
    The Nylas API supports creating, updating and deleting webhooks and this PR enables support for these additional operations. We also provide a couple of helper modules with strings representing various webhook status and triggervalues.

--- examples/plain-ruby/webhooks.rb
+++ examples/plain-ruby/webhooks.rb
@@ -7,8 +7,23 @@ api = Nylas::API.new(app_id: ENV['NYLAS_APP_ID'], app_secret: ENV['NYLAS_APP_SEC
 
 # Webhooks can be retrieved as a collection
 demonstrate { api.webhooks.map(&:to_h) }
+
 # Or independently
 example_webhook = api.webhooks.first
 demonstrate { api.webhooks.find(example_webhook.id).to_h }
 
+# Create a webhook
+demonstrate do
+  api.webhooks.create(
+    callback_url: ENV['NYLAS_WEBHOOK_URL'],
+    state: WebhookState::ACTIVE,
+    triggers: [WebhookTrigger::EVENT_CREATED],
+  )
+end
 
+# Update the status of the webhook
+created_webhook = api.webhooks.last
+demonstrate { created_webhook.update(WebhookState::INACTIVE) }
+
+# Delete webhook
+demonstrate { created_webhook.destroy }
--- lib/nylas/webhook.rb
+++ lib/nylas/webhook.rb
@@ -1,21 +1,94 @@
 # frozen_string_literal: true
 
+module WebhookState
+  # Module representing the possible 'state' values in a Webhook
+  # @see https://developer.nylas.com/docs/api#post/a/client_id/webhooks
+
+  ACTIVE = "active"
+  INACTIVE = "inactive"
+end
+
+module WebhookTrigger
+  # Module representing the possible 'trigger' values in a Webhook
+  # @see https://developer.nylas.com/docs/api#post/a/client_id/webhooks
+
+  ACCOUNT_CONNECTED = "account.connected"
+  ACCOUNT_RUNNING = "account.running"
+  ACCOUNT_STOPPED = "account.stopped"
+  ACCOUNT_INVALID = "account.invalid"
+  ACCOUNT_SYNC_ERROR = "account.sync_error"
+  MESSAGE_CREATED = "message.created"
+  MESSAGE_OPENED = "message.opened"
+  MESSAGE_LINK_CLICKED = "message.link_created"
+  MESSAGE_UPDATED = "message.updated"
+  MESSAGE_BOUNCED = "message.bounced"
+  THREAD_REPLIED = "thread.replied"
+  CONTACT_CREATED = "contact.created"
+  CONTACT_UPDATED = "contact.updated"
+  CONTACT_DELETED = "contact.deleted"
+  CALENDAR_CREATED = "calendar.created"
+  CALENDAR_UPDATED = "calendar.updated"
+  CALENDAR_DELETED = "calendar.deleted"
+  EVENT_CREATED = "event.created"
+  EVENT_UPDATED = "event.updated"
+  EVENT_DELETED = "event.deleted"
+  JOB_SUCCESSFUL = "job.successful"
+  JOB_FAILED = "job.failed"
+end
+
 module Nylas
   # Represents a webhook attached to your application.
   # @see https://docs.nylas.com/reference#webhooks
   class Webhook
     include Model
-    allows_operations(listable: true, showable: true)
-    attribute :id, :string
-    attribute :application_id, :string
+    allows_operations(creatable: true, listable: true, showable: true, updatable: true,
+                      destroyable: true)
+    attribute :id, :string, read_only: true
+    attribute :application_id, :string, read_only: true
 
     attribute :callback_url, :string
     attribute :state, :string
-    attribute :version, :string
+    attribute :version, :string, read_only: true
     has_n_of_attribute :triggers, :string
+
+    STATE = [:inactive].freeze
+
+    def save
+      result = if persisted?
+                 update_call(update_payload)
+               else
+                 create
+               end
+
+      attributes.merge(result)
+    end
+
+    def save_all_attributes
+      save
+    end
+
+    def update(**data)
+      raise ArgumentError, "Only 'state' is allowed to be updated" if data.length > 1 || !data.key?(:state)
+
+      attributes.merge(**data)
+      payload = JSON.dump(data)
+      update_call(payload)
+
+      true
+    end
+
+    def update_all_attributes(**data)
+      update(**data)
+    end
 
     def self.resources_path(api:)
       "/a/#{api.app_id}/webhooks"
+    end
+
+    private
+
+    def update_payload
+      JSON.dump({ state: state })
     end
   end
 end
