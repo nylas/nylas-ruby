6f56da081ff5a0dae153db602b1d3f7b513643c2
Tys von Gaza <tys.von.gaza@aeon.health>
2023-04-04 07:34:39 -0600

    Allow native authentication to return the  full response like the exchange_code_for_token (#411)
    Adds a return_full_response argument to the Api#authenticate method, along with it an implementation in NativeAuthentication which mirrors the code in the exchange_code_for_token method.

--- lib/nylas/native_authentication.rb
+++ lib/nylas/native_authentication.rb
@@ -11,13 +11,13 @@ module Nylas
     end
 
     def authenticate(name:, email_address:, provider:, settings:, reauth_account_id: nil,
-                     scopes: nil)
+                     scopes: nil, return_full_response: false)
       scopes ||= %w[email calendar contacts]
       scopes = scopes.join(",") unless scopes.is_a?(String)
       code = retrieve_code(name: name, email_address: email_address, provider: provider,
                            settings: settings, reauth_account_id: reauth_account_id, scopes: scopes)
 
-      exchange_code_for_access_token(code)
+      exchange_code_for_access_token(code, return_full_response)
     end
 
     private
@@ -30,10 +30,10 @@ module Nylas
       response[:code]
     end
 
-    def exchange_code_for_access_token(code)
+    def exchange_code_for_access_token(code, return_full_response)
       payload = { client_id: api.client.app_id, client_secret: api.client.app_secret, code: code }
       response = api.execute(method: :post, path: "/connect/token", payload: JSON.dump(payload))
-      response[:access_token]
+      return_full_response ? response : response[:access_token]
     end
   end
 end
--- spec/nylas/native_authentication_spec.rb
+++ spec/nylas/native_authentication_spec.rb
@@ -3,9 +3,63 @@
 require "spec_helper"
 
 # rubocop:disable RSpec/MessageSpies
+# rubocop:disable Rspec/NestedGroups
+# rubocop:disable RSpec/MultipleMemoizedHelpers
 
 describe Nylas::NativeAuthentication do
   describe "#authenticate" do
+    context "when called return_full_response parameter" do
+      let(:client) do
+        Nylas::HttpClient.new(app_id: "not-real", app_secret: "also-not-real",
+                              access_token: "seriously-unreal")
+      end
+      let(:api) { Nylas::API.new(client: client) }
+      let(:scopes) { "email,calendar,contacts" }
+      let(:scopes_array) { %w[email calendar contacts] }
+      let(:token_response) { { access_token: "fake-token", other_data: "other-data" } }
+
+      shared_examples "a successful authentication" do |return_full_response|
+        it "returns the expected value" do
+          expect(client).to receive(:execute).with(
+            method: :post, path: "/connect/authorize",
+            payload: be_json_including("scopes" => scopes)
+          ).and_return(code: 1234)
+          expect(client).to receive(:execute).with(
+            method: :post, path: "/connect/token",
+            payload: be_json_including("code" => 1234)
+          ).and_return(token_response)
+          expect(
+            api.authenticate(
+              name: "fake",
+              email_address: "fake@example.com",
+              provider: :gmail,
+              settings: {},
+              scopes: scopes_array,
+              return_full_response: return_full_response
+            )
+          ).to eql(expected_result)
+        end
+      end
+
+      context "when return_full_response is not set" do
+        let(:expected_result) { "fake-token" }
+
+        it_behaves_like "a successful authentication"
+      end
+
+      context "when return_full_response is false" do
+        let(:expected_result) { "fake-token" }
+
+        it_behaves_like "a successful authentication", false
+      end
+
+      context "when return_full_response is true" do
+        let(:expected_result) { token_response }
+
+        it_behaves_like "a successful authentication", true
+      end
+    end
+
     it "sets all scopes by default" do
       client = Nylas::HttpClient.new(
         app_id: "not-real",
@@ -116,3 +170,5 @@ describe Nylas::NativeAuthentication do
 end
 
 # rubocop:enable RSpec/MessageSpies
+# rubocop:enable Rspec/NestedGroups
+# rubocop:enable RSpec/MultipleMemoizedHelpers
