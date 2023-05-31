13ca5715f0c17d5ebbf6a3e4437b49b581559882
Albert T <albert.t@nylas.com>
2023-05-31 17:29:16 -0400

    update webhook triggers

--- lib/nylas/resources/webhooks.rb
+++ lib/nylas/resources/webhooks.rb
@@ -8,30 +8,18 @@ module Nylas
     # Module representing the possible 'trigger' values in a Webhook
     # @see https://developer.nylas.com/docs/api#post/a/client_id/webhooks
 
-    ACCOUNT_CONNECTED = "account.connected"
-    ACCOUNT_RUNNING = "account.running"
-    ACCOUNT_STOPPED = "account.stopped"
-    ACCOUNT_INVALID = "account.invalid"
-    ACCOUNT_SYNC_ERROR = "account.sync_error"
-    MESSAGE_CREATED = "message.created"
-    MESSAGE_OPENED = "message.opened"
-    MESSAGE_LINK_CLICKED = "message.link_clicked"
-    MESSAGE_UPDATED = "message.updated"
-    MESSAGE_BOUNCED = "message.bounced"
-    THREAD_REPLIED = "thread.replied"
-    CONTACT_CREATED = "contact.created"
-    CONTACT_UPDATED = "contact.updated"
-    CONTACT_DELETED = "contact.deleted"
     CALENDAR_CREATED = "calendar.created"
     CALENDAR_UPDATED = "calendar.updated"
     CALENDAR_DELETED = "calendar.deleted"
     EVENT_CREATED = "event.created"
     EVENT_UPDATED = "event.updated"
     EVENT_DELETED = "event.deleted"
-    JOB_SUCCESSFUL = "job.successful"
-    JOB_FAILED = "job.failed"
-    JOB_DELAYED = "job.delayed"
-    JOB_CANCELLED = "job.cancelled"
+    GRANT_CREATED = "grant.created"
+    GRANT_UPDATED = "grant.updated"
+    GRANT_DELETED = "grant.deleted"
+    GRANT_EXPIRED = "grant.expired"
+    MESSAGE_SEND_SUCCESS = "message.send_success"
+    MESSAGE_SEND_FAILED = "message.send_failed"
   end
 
   # Webhooks
