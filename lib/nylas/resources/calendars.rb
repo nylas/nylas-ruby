bcd2b2c7f547b11ac59c6cd2b18aba3f11f8cfa6
Mostafa Rashed <17770919+mrashed-dev@users.noreply.github.com>
2023-10-11 16:30:11 -0400

    Add support for free-busy endpoint

--- lib/nylas/resources/calendars.rb
+++ lib/nylas/resources/calendars.rb
@@ -19,11 +19,23 @@ module Nylas
 
     # Checks multiple calendars to find available time slots for a single meeting.
     #
-    # @param request_body [Hash, nil] Request body to pass to the request.
+    # @param request_body Hash Request body to pass to the request.
     # @return [Array(Hash, String)] Availability object and API request ID.
-    def get_availability(request_body: nil)
+    def get_availability(request_body:)
       post(
         path: "#{api_uri}/v3/calendars/availability",
+        request_body: request_body
+      )
+    end
+
+    # Get the free/busy schedule for a list of email addresses.
+    #
+    # @param identifier [str] The identifier of the grant to act upon.
+    # @param request_body [Hash] Request body to pass to the request.
+    # @return [Array(Array(Hash), String)] The free/busy response.
+    def get_free_busy(identifier:, request_body:)
+      post(
+        path: "#{api_uri}/v3/grants/#{identifier}/calendars/availability",
         request_body: request_body
       )
     end
