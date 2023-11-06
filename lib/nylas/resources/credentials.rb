70a39e7c8177ae168285be4d87c71a8e90cd77a5
Mostafa Rashed <17770919+mrashed-dev@users.noreply.github.com>
2023-11-06 17:19:28 -0500

    TW-2422 Add credential API support

--- lib/nylas/resources/credentials.rb
+++ lib/nylas/resources/credentials.rb
@@ -5,29 +5,20 @@ require_relative "../handler/api_operations"
 
 module Nylas
   # Nylas Connectors API
-  class Connectors < Resource
+  class Credentials < Resource
     include ApiOperations::Get
     include ApiOperations::Post
     include ApiOperations::Put
     include ApiOperations::Delete
 
-    # Access the Credentials API
-    attr_reader :credentials
-
-    # Initializes Connectors.
-    def initialize(sdk_instance)
-      super(sdk_instance)
-
-      @credentials = Credentials.new(sdk_instance)
-    end
-
-    # Return all connectors.
+    # Return all credentials.
     #
+    # @param provider [String] The provider associated to the credential to list from
     # @param query_params [Hash, nil] Query params to pass to the request.
-    # @return [Array(Array(Hash), String)] The list of connectors and API Request ID.
-    def list(query_params: nil)
+    # @return [Array(Array(Hash), String)] The list of credentials and API Request ID.
+    def list(provider:, query_params: nil)
       get(
-        path: "#{api_uri}/v3/connectors",
+        path: "#{api_uri}/v3/connectors/#{provider}/creds",
         query_params: query_params
       )
     end
@@ -35,32 +26,35 @@ module Nylas
     # Return a connector.
     #
     # @param provider [String] The provider associated to the connector to retrieve.
+    # @param credential_id [String] The id of the credentials to retrieve.
     # @return [Array(Hash, String)] The connector and API request ID.
-    def find(provider:)
+    def find(provider:, credential_id:)
       get(
-        path: "#{api_uri}/v3/connectors/#{provider}"
+        path: "#{api_uri}/v3/connectors/#{provider}/creds/#{credential_id}"
       )
     end
 
     # Create a connector.
     #
+    # @param provider [String] The provider associated to the credential being created
     # @param request_body [Hash] The values to create the connector with.
     # @return [Array(Hash, String)] The created connector and API Request ID.
-    def create(request_body:)
+    def create(provider:, request_body:)
       post(
-        path: "#{api_uri}/v3/connectors",
+        path: "#{api_uri}/v3/connectors/#{provider}/creds",
         request_body: request_body
       )
     end
 
     # Update a connector.
     #
-    # @param provider [String] The provider associated to the connector to update.
+    # @param provider [String] The provider associated to the connector to update from.
+    # @param credential_id [String] The id of the credentials to update.
     # @param request_body [Hash] The values to update the connector with
     # @return [Array(Hash, String)] The updated connector and API Request ID.
-    def update(provider:, request_body:)
+    def update(provider:, credential_id:, request_body:)
       put(
-        path: "#{api_uri}/v3/connectors/#{provider}",
+        path: "#{api_uri}/v3/connectors/#{provider}/creds/#{credential_id}",
         request_body: request_body
       )
     end
@@ -68,10 +62,11 @@ module Nylas
     # Delete a connector.
     #
     # @param provider [String] The provider associated to the connector to delete.
+    # @param credential_id [String] The id of the credentials to delete.
     # @return [Array(TrueClass, String)] True and the API Request ID for the delete operation.
-    def destroy(provider:)
+    def destroy(provider:, credential_id:)
       _, request_id = delete(
-        path: "#{api_uri}/v3/connectors/#{provider}"
+        path: "#{api_uri}/v3/connectors/#{provider}/creds/#{credential_id}"
       )
 
       [true, request_id]
