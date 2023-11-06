7cb63cde67162f8d3f47c1de6f8be5279357b815
Mostafa Rashed <17770919+mrashed-dev@users.noreply.github.com>
2023-11-06 15:42:31 -0500

    TW-2420 Native/Custom auth support

--- lib/nylas/resources/grants.rb
+++ lib/nylas/resources/grants.rb
@@ -6,7 +6,7 @@ require_relative "../handler/admin_api_operations"
 module Nylas
   # Grants
   class Grants < Resource
-    include AdminApiOperations::Create
+    include ApiOperations::Post
     include AdminApiOperations::Update
     include AdminApiOperations::List
     include AdminApiOperations::Destroy
@@ -15,6 +15,17 @@ module Nylas
     # Initializes Grants.
     def initialize(sdk_instance)
       super("grants", sdk_instance)
+    end
+
+    # Create a Grant via Custom Authentication.
+    #
+    # @param request_body [Hash] The values to create the Grant with.
+    # @return [Array(Hash, String)] Created grant and API Request ID.
+    def create(request_body)
+      post(
+        path: "#{api_uri}/v3/#{resource_name}/custom",
+        request_body: request_body
+      )
     end
   end
 end
