99deaf590a7ebba29fa462b5629b65c4da24543b
Mostafa Rashed <17770919+mrashed-dev@users.noreply.github.com>
2023-11-09 12:47:07 -0500

    add support for sending + scheduling messages

--- lib/nylas/resources/messages.rb
+++ lib/nylas/resources/messages.rb
@@ -3,11 +3,13 @@
 require_relative "resource"
 require_relative "smart_compose"
 require_relative "../handler/api_operations"
+require_relative "../utils/file_utils"
 
 module Nylas
   # Nylas Messages API
   class Messages < Resource
     include ApiOperations::Get
+    include ApiOperations::Post
     include ApiOperations::Put
     include ApiOperations::Delete
 
@@ -70,6 +72,56 @@ module Nylas
       )
 
       [true, request_id]
+    end
+
+    # Send a message.
+    #
+    # @param identifier [String] Grant ID or email account from which to delete an object.
+    # @param request_body [Hash] The values to create the message with.
+    # @return [Array(Hash, String)] The sent message and the API Request ID.
+    def send(identifier:, request_body:)
+      form_body, opened_files = FileUtils.build_form_request(request_body)
+
+      response = post(
+        path: "#{api_uri}/v3/grants/#{identifier}/messages/send",
+        request_body: form_body
+      )
+
+      opened_files.each(&:close)
+
+      response
+    end
+
+    # Retrieve your scheduled messages.
+    #
+    # @param identifier [String] Grant ID or email account from which to find the scheduled message from.
+    # @param schedule_id [String] The id of the scheduled message to stop.
+    # @return [Array(Hash, String)] The list of scheduled messages and the API Request ID.
+    def list_scheduled_messages(identifier:, schedule_id:)
+      get(
+        path: "#{api_uri}/v3/grants/#{identifier}/messages/schedules/#{schedule_id}"
+      )
+    end
+
+    # Retrieve your scheduled messages.
+    #
+    # @param identifier [String] Grant ID or email account from which to list the scheduled messages from.
+    # @return [Array(Hash, String)] The scheduled message and the API Request ID.
+    def find_scheduled_messages(identifier:)
+      get(
+        path: "#{api_uri}/v3/grants/#{identifier}/messages/schedules"
+      )
+    end
+
+    # Stop a scheduled message.
+    #
+    # @param identifier [String] Grant ID or email account from which to list the scheduled messages from.
+    # @param schedule_id [String] The id of the scheduled message to stop..
+    # @return [Array(Hash, String)] The scheduled message and the API Request ID.
+    def stop_scheduled_messages(identifier:, schedule_id:)
+      delete(
+        path: "#{api_uri}/v3/grants/#{identifier}/messages/schedules/#{schedule_id}"
+      )
     end
   end
 end
