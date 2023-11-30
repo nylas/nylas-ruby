506b71c68955bd5ce633037c6409a50ab389a41e
Mostafa Rashed <17770919+mrashed-dev@users.noreply.github.com>
2023-11-30 17:15:50 -0500

    Add test for search collection

---
+++ spec/nylas/search_collection_spec.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+
+require "spec_helper"
+
+describe Nylas::SearchCollection do
+  let(:api) { FakeAPI.new }
+
+  describe "#count" do
+    it "Returns an enumerable for a single page of results, filtered by `offset` and `limit` and `where`" do
+      allow(api).to receive(:execute)
+        .with(
+          auth_method: Nylas::HttpClient::AuthMethod::BEARER,
+          method: :get,
+          path: "/collection/search",
+          query: { limit: 100, offset: 0 },
+          headers: {}
+        ).and_return([{ id: "1234" }])
+
+      collection = described_class.new(model: FullModel, api: api)
+
+      results = collection.each.to_a
+
+      expect(results.count).to be 1
+    end
+  end
+end
