1a9de6ccaa87ba24360e2df3668759c2589831bf
Subash Pradhan <subash.p@macbook-pro.home>
2024-10-24 14:33:11 +0200

    Use attr_reader and adding comment for documentation

--- lib/nylas/resources/scheduler.rb
+++ lib/nylas/resources/scheduler.rb
@@ -6,38 +6,26 @@ require_relative "./bookings"
 require_relative "./availability"
 
 module Nylas
+  # Nylas Scheduler API
+  # This class provides access to the Scheduler resources, including
+  # configurations, bookings, sessions, and availability.
+  #
+  # @attr_reader [Nylas::Configurations] configurations The Scheduler configurations resource for your Nylas application.
+  # @attr_reader [Nylas::Bookings] bookings The Scheduler bookings resource for your Nylas application.
+  # @attr_reader [Nylas::Sessions] sessions The Scheduler sessions resource for your Nylas application.
+  # @attr_reader [Nylas::Availability] availability The Scheduler availability resource for your Nylas application.
   class Scheduler
+    attr_reader :configurations, :sessions, :bookings, :availability
+
+    # Initializes the Scheduler class.
+    #
+    # @param api_client [APIClient] The Nylas API client instance for making requests.
     def initialize(api_client)
       @api_client = api_client
       @configurations = Configurations.new(@api_client)
       @bookings = Bookings.new(@api_client)
       @sessions = Sessions.new(@api_client)
       @availability = Availability.new(@api_client)
-    end
-
-    # The configuration resources for your Nylas application.
-    # @return [Nylas::Scheduler::Confiugrations] Scheduler configuration resources
-    # for your Nylas application.
-    def configurations
-      @configurations
-    end
-
-    # The Session resources for your Nylas application.
-    # @return [Nylas::Scheduler::Sessions] Scheduler session resources for your Nylas application.
-    def sessions
-      @sessions
-    end
-    
-    # The Booking resources for your Nylas application.
-    # @return [Nylas::Scheduler::Bookings] Scheduler booking resources for your Nylas application.
-    def bookings
-      @bookings
-    end
-
-    # The availability resources for your Nylas application.
-    # @return [Nylas::Scheduler::Availability] Scheduling availability resources for your Nylas application.
-    def availability
-      @availability
     end
   end
 end
